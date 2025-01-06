4c081ef482e9dedc96a8afcdbef2fdd0e4573094
barakatimothy <timothybaraka39@gmail.com>
2025-01-07 00:11:36 +0300

    Data types java

---
+++ Datatypes.java
@@ -0,0 +1,10 @@
+public class Datatypes{
+    public static void main(String []args){
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D'; 
+        boolean myBool = true; 
+        String myText = "Hello";
+        System.out.println(myFloatNum);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
-public class Main {
-    public static void main(String[] args){
-        int myNum = 4 ;
-
-        System.out.println(myNum);
+public class Main{
+    public static void main(String [] args){
+        System.out.println("Hello, world");
     }
+
 }
